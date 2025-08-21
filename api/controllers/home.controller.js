86aeddb89eb2ed6d4b3b7ff445f185fa34d56e79
thanhnt0208 <thanhnguyen28216@gmail.com>
2025-08-21 08:06:19 +0700

    Update home.controller.js

--- api/controllers/home.controller.js
+++ api/controllers/home.controller.js
@@ -1,29 +1,32 @@
 const db = require("../config/db.js");
 
 const getHomeProducts = (req, res) => {
-  const spbanchay = `SELECT id_sp, ten, gia, gia_km, hinh, slug ,luot_mua ,luot_xem
-                    FROM sanpham 
-                    WHERE an_hien = 1 
-                    ORDER BY luot_mua DESC 
-                    LIMIT 4`;
+ const spbanchay = `
+  SELECT id_sp, ten, gia, gia_km, hinh, slug, luot_mua, luot_xem
+  FROM sanpham 
+  WHERE an_hien = 1 
+  ORDER BY luot_mua DESC
+`;
 
-  const spview = `SELECT id_sp, ten, gia, gia_km, hinh, slug ,luot_xem, luot_mua
-                FROM sanpham 
-                WHERE an_hien = 1 
-                ORDER BY luot_xem + luot_mua DESC 
-                LIMIT 4`;
+const spview = `
+  SELECT id_sp, ten, gia, gia_km, hinh, slug, luot_xem, luot_mua
+  FROM sanpham 
+  WHERE an_hien = 1 
+  ORDER BY luot_xem + luot_mua DESC
+`;
 
-  const spmoi = `SELECT id_sp, ten, gia, gia_km, hinh, slug, luot_xem, luot_mua
-                  FROM sanpham
-                  WHERE an_hien = 1
-                  ORDER BY (luot_xem + luot_mua) DESC
-                  LIMIT 4;
-                  `;
-  const spgiamgia = `SELECT id_sp, ten, gia, gia_km, hinh, slug , luot_xem, luot_mua
-                     FROM sanpham 
-                     WHERE an_hien = 1 AND gia_km < gia 
-                     ORDER BY ngay ASC 
-                     LIMIT 8`;
+const spmoi = `
+  SELECT id_sp, ten, gia, gia_km, hinh, slug, luot_xem, luot_mua
+  FROM sanpham
+  WHERE an_hien = 1
+  ORDER BY (luot_xem + luot_mua) DESC
+`;
+
+const spgiamgia = `SELECT id_sp, ten, gia, gia_km, hinh, slug , luot_xem, luot_mua
+    FROM sanpham 
+    WHERE an_hien = 1 AND gia_km < gia 
+    ORDER BY ngay ASC 
+    LIMIT 8`;
 
   db.query(spbanchay, (err, hot) => {
     if (err) return res.status(500).json({ error: err.message });
