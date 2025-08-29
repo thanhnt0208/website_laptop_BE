f13dc10e15c10f2f463247ae2a81f0e8416137ab
thanhnt0208 <thanhnguyen28216@gmail.com>
2025-08-29 07:56:11 +0700

    Create upload.js

---
+++ api/middleware/upload.js
@@ -0,0 +1,21 @@
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const dir = path.join(__dirname, "../uploads/returns");
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
+    }
+    cb(null, dir);
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage: storage });
+
+module.exports = upload;
